1b9e4ac829054169d2c7fb41e890fecbebb9052b
József Tarjányi <tarjanyi.jozsef@freemail.hu>
2018-08-13 14:38:07 +0200

    #02 : Ermehamisitas

---
+++ 02Nap/Ermehamisitas/03Ermehamisitas/Coin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _03Ermehamisitas
+{
+   public class Coin
+    {
+
+        Random random = new Random();
+
+        public int collect()
+        {
+            return random.Next(0, 2);
+        }
+    }
+}
---
+++ 02Nap/Ermehamisitas/03Ermehamisitas/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _03Ermehamisitas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var coin = new Coin();
+
+            Console.WriteLine("Az érmefeldobás eredménye:");
+            for (int i = 0; i < 100; i++)
+            {
+
+                Console.Write($"{coin.collect()},");
+            }
+
+            Console.WriteLine("Hello World!");
+
+
+            Console.ReadLine();
+        }
+    }
+}
